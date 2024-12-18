908fa87a1611b954e9f2ef6a79bad47d814f6f90
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-12-18 13:03:51 -0300

    definição de $request

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+$request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/':
@@ -19,6 +20,5 @@ switch ($request) {
 
     default:
         http_response_code(404);
-        echo "Página não encontrada.";
         break;
 }
